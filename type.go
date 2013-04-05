b8243466e01d3e4b6f23d9ebc99b7913f412d2a5
Seth Bunce <seth.bunce@gmail.com>
2013-04-04 20:19:35 -0700

    Added software license.

--- type.go
+++ type.go
@@ -1,3 +1,6 @@
+// Copyright 2013 Seth Bunce. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 package bson
 
 // Wire types.
